b59711624eb8ce4888f5fe19c243a0ed9b511a49
Reine222 <guelanin.koulai@uvci.edu.ci>
2019-10-04 15:39:25 +0000

    model
    models

--- exoApi/api/models.py
+++ exoApi/api/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Categories(models.Model):
-    
-    
+    nom = models.CharField(_(""), max_length=50)
+    description = models.TextField(_(""))
+    image = models.ImageField(_(""), upload_to=None, height_field=None, width_field=None, max_length=None)
+    statut = models.BooleanField(_(""))
+    date
 
     
 
